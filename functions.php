c231fddf1ad3677d935ca8edb64e18032b6c0e09
José Olinda <joseolindad@yahoo.com.br>
2019-07-30 19:08:10 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,3 +7,14 @@ function __e($input)
     $input = htmlspecialchars($input);
     return $input;
 }
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = __e($_POST["nome"]);
+    $email = __e($_POST["email"]);
+    $sexo = __e($_POST["sexo"]);
+    $tipo_mensagem = __e($_POST["tipo-mensagem"]);
+    $mensagem = __e($_POST["mensagem"]);
+
+    $mostrarMensagem = true;
+}
